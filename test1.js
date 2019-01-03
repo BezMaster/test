c5c4df6beb1a529319ffa5127067e482d9838755
sebastien.chesneau <sebastien.chesneau@ama.bzh>
2019-01-03 21:52:22 +0100

    coco

--- test1.js
+++ test1.js
@@ -1,9 +1,6 @@
 var toto = "test";
 var teto = "test";
-<<<<<<< branch-test
 const tti = 'trtr';
 const titi = 'fgfgf';
-=======
 //fgfgffg
 //fl;f;lg;l
->>>>>>> dev test
--- totot.js
+++ totot.js
@@ -1,12 +1,9 @@
 gogo / / / fldef
 var totot ))
-<<<<<<< branch-test
 //dfgfgf
 /*/fg:fglf;l;f
 ghggh
 ghgghhg
 ghgh
 gh
-=======
 const gigi = '';
->>>>>>> dev test
