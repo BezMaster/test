c446b8b9aa399c0efe0b701bfd23118528132076
sebastien.chesneau <sebastien.chesneau@ama.bzh>
2019-03-10 22:02:05 +0100

    'fuck'

--- test1.js
+++ test1.js
@@ -1,9 +1,6 @@
-<<<<<<< HEAD
 var devt = 'sdfde'
 var gigi = 'fddf'
-=======
 var toto = "test";
 var teto = "test2";
 var teto = "test3";
 var teto = "test4";
->>>>>>> h1
--- totot.js
+++ totot.js
@@ -1,7 +1,4 @@
-<<<<<<< HEAD
 var dev =  "trrii"
 var dev2 = "riri"
-=======
 const toto = 15;
 const toto = 15;
->>>>>>> h1
