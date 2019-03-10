9f44b6c34cda016c2a046cbf9a5f037767265a90
sebastien.chesneau <sebastien.chesneau@ama.bzh>
2019-03-10 22:04:46 +0100

    add  one line

--- test1.js
+++ test1.js
@@ -4,3 +4,4 @@ var toto = "test";
 var teto = "test2";
 var teto = "test3";
 var teto = "test4";
+var teto = "test5";
--- totot.js
+++ totot.js
@@ -2,3 +2,4 @@ var dev =  "trrii"
 var dev2 = "riri"
 const toto = 15;
 const toto = 15;
+const toti = 16;
